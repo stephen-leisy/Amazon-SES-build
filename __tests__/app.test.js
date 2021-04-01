81ca4c4e982d7ffcbb5d60612efbf76dca4d9cc9
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-04-01 12:06:38 -0700

    wrote passing tests for all crud enpoints and one test for SES email system.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,10 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Order = require('../lib/models/Order');
+const SES = require('../lib/utils/SES');
+
+jest.mock('../lib/utils/SES.js');
 
 describe('Amazon SES build routes', () => {
   beforeEach(() => {
@@ -11,17 +15,60 @@ describe('Amazon SES build routes', () => {
   const testUser = {
     name: 'Steph Leisy',
     email: 'stleisy@gmail.com',
+    itemId: 1,
     quantity: 1,
   };
 
+  let userOrder;
+  beforeEach(async () => {
+    userOrder = await request(app).post('/api/v1/orders').send(testUser);
+  });
+
   it('it takes an order quantity and puts it into the database and sends an email', async () => {
-    const results = await request(app).post('/api/v1/orders').send(testUser);
-    console.log('results:', results.body)
+
+    expect(SES.sendingEmail).toHaveBeenCalledTimes(1);
+    expect(userOrder.body).toEqual({
+      id: '1',
+      name: 'Steph Leisy',
+      email: 'stleisy@gmail.com',
+      itemId: 1,
+      quantity: 1,
+    });
+  });
+  it('displays all orders', async () => {
+    const results = await request(app).get('/api/v1/orders');
+    expect(results.body).toEqual([
+      {
+        id: '1',
+        name: 'Steph Leisy',
+        email: 'stleisy@gmail.com',
+        itemId: 1,
+        quantity: 1,
+      },
+    ]);
+  });
+  it('displays a single order by ID', async () => {
+    const results = await request(app).get('/api/v1/orders/1');
     expect(results.body).toEqual({
       id: '1',
       name: 'Steph Leisy',
       email: 'stleisy@gmail.com',
+      itemId: 1,
       quantity: 1,
     });
   });
+  it('updates an order quantity by ID', async () => {
+    const results = await request(app).put('/api/v1/orders/1').send({ quantity: 2 });
+    expect(results.body).toEqual({ id: '1',
+    name: 'Steph Leisy',
+    email: 'stleisy@gmail.com',
+    itemId: 1,
+    quantity: 2,})
+  })
+  it('deletes an order by its id', async () => {
+    
+    await request(app).delete('/api/v1/orders/1');
+    const results = await request(app).get('/api/v1/orders');
+    expect(results.body).toEqual([]);
+  })
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS orders;
 
 CREATE TABLE orders (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR(256) NOT NULL,
+    users_name VARCHAR(256) NOT NULL,
     email VARCHAR(256) NOT NULL,
+    item_id INTEGER NOT NULL,
     quantity INTEGER CHECK (quantity > 0)
 );
